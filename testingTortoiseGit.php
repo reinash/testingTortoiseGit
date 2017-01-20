35ac62668bec4434c7f79d006b95c516fca5cd45
reinash <johncedriccarandang@gmail.com>
2017-01-20 08:06:58 +0800

    4th commit - added instructions on how to use torgit

--- testingTortoiseGit.php
+++ testingTortoiseGit.php
@@ -3,4 +3,12 @@
 echo "this is a demo of tortoise git!";
 echo "let's see what happens when you commit the second time";
 echo "this is a 3rd commit!";
+
+//Steps to Use Tortoise Git
+//1 right click folder containing your files
+//2 click Git Commit -> Master
+//3 type a comment of the change then click commit button
+//4 now if u want to push this change in github, open pageant from TortoiseGit folder at startup to initialize your connection
+//5 now go to github and copy the sshkey from your repository
+//6 now finally rightclick the folder in your local files and select Git Sync, paste your ssh key there and you can now pull or push from github. enjoy! 
 ?>
