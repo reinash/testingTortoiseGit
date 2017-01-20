3fa7a071998558933da378f719f07e43ea0a8a90
reinash <johncedriccarandang@gmail.com>
2017-01-20 08:24:15 +0800

    5th commit - added instructions on how to clone a repo from github and modified some instructions on how to use Tortoise Git

--- testingTortoiseGit.php
+++ testingTortoiseGit.php
@@ -5,10 +5,19 @@ echo "let's see what happens when you commit the second time";
 echo "this is a 3rd commit!";
 
 //Steps to Use Tortoise Git
+//0 right click folder to use and choose create a repository here
 //1 right click folder containing your files
 //2 click Git Commit -> Master
+//2.1 be sure to include all files you want to commit by checking them all below the comment box
 //3 type a comment of the change then click commit button
 //4 now if u want to push this change in github, open pageant from TortoiseGit folder at startup to initialize your connection
-//5 now go to github and copy the sshkey from your repository
-//6 now finally rightclick the folder in your local files and select Git Sync, paste your ssh key there and you can now pull or push from github. enjoy! 
+//5 now go to github and copy the remotekey from your repository
+//6 now finally rightclick the folder in your local files and select Git Sync, paste your remote key there and you can now pull or push from github. enjoy! 
+
+//Steps to clone a repo from github
+//1 create again a folder where you want to place the repo from github
+//2 right click folder to use and choose create a repository here
+//3 right click folder and choose Git Sync
+//4 now click pull and be sure you have input the right remote key from repo on github
+//5 DONE! It is cloned!
 ?>
